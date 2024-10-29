be1f1f2a7ebc64fc796ad85414f2b12dbd062448
Eduardo <eduardopinedaalmeda@gmail.com>
2024-10-29 11:46:38 -0600

    modifique el archivo de factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include <iostream>
-using namespace std;
+
 int Factorial(int numero) {
     int factorial = 1;
     for (int i = 1; i <= numero; ++i) {
@@ -8,17 +7,62 @@ int Factorial(int numero) {
     }
     return factorial;
 }
-int main() {
-    int num;
-    cout << "Ingrese un numero: ";
-    cin >>num;
-    if (num < 0) {
-        cout << "No se puede calcular el factorial de un numero negativo. "<<endl;
+
+using namespace std;
+
+
+int main()
+{   
+    int opcion;
+    cout << "elige la opcion que desees utilizar 1 es cajero cualquier otra opcion es el factorial" << endl;
+    cin >> opcion;
+    if (opcion == 1) {
+
+        int opcion;
+        int repetir;
+        int saldo = 20000, dineraso;
+        do {
+            cout << "elige la opcion que desees utilizar " << endl;
+            cout << "1:Consultar saldo, 2: Retirar efectivo" << endl;
+            cin >> opcion;
+            cout << endl;
+            switch (opcion) {
+
+            case 1:
+                cout << "Consultar saldo: " << endl;
+                cout << "Tu saldo es: " << endl;
+                cout << saldo;
+                cout << endl;
+                break;
+            case 2:
+                cout << "Retirar efectivo: " << endl;
+                cout << "cuanto saldo deseas retirar?: " << endl;
+                cin >> dineraso;
+                saldo = saldo - dineraso;
+                cout << endl;
+                break;
+            default:
+                cout << "Opción no válida." << std::endl;
+                break;
+            }
+            cout << "desdeas regresar al menu para consultar tu saldo?:" << endl << "pulsa cualquier tecla para continuar pulsa 0 para finalizar " << endl;
+            cin >> repetir;
+            cout << endl;
+        } while (repetir != 0);
+
+        cout << "Estamos fuera " << endl;
+
     }
     else {
-        int resultado = Factorial(num);
-        cout << "El factorial de: " << num << "es: " << resultado << endl;
+        int num;
+        cout << "Ingrese un numero: ";
+        cin >> num;
+        if (num < 0) {
+            cout << "No se puede calcular el factorial de un numero negativo. " << endl;
+        }
+        else {
+            int resultado = Factorial(num);
+            cout << "El factorial de: " << num << "es: " << resultado << endl;
+        }
     }
-
-    return 0;
 }
